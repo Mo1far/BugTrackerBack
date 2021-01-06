bd34fb6f621c7108a596881c76f55ad6d4a66e20
Mo1far <vladuslav09@gmail.com>
2021-01-06 18:25:18 +0200

    improve configs

--- bot/config.py
+++ bot/config.py
@@ -9,12 +9,6 @@ load_dotenv(ENV_FILE)
 
 TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 
-DB_USERNAME = os.environ.get('DB_USERNAME')
-DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT')
-DB_NAME = os.environ.get('DB_NAME')
-DB_PASSWORD = os.environ.get('DB_PASSWORD')
-
 ADMIN_CHAT_ID = os.environ.get('ADMIN_CHAT_ID')
 
 DEFAULT_RATE_LIMIT = 2
